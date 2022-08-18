97f8fe4b6ec165d3ea8d01007a2cbe3e5f80b89d
TMDStudios <tmdstudioscontact@gmail.com>
2022-08-18 15:34:00 -0400

    Constraint Layout

--- app/src/main/java/com/example/catmeme/ImagePage.kt
+++ app/src/main/java/com/example/catmeme/ImagePage.kt
@@ -20,6 +20,8 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 
 @Composable
 fun ImagePage(){
@@ -27,44 +29,65 @@ fun ImagePage(){
         .fillMaxSize()
         .padding(top = 100.dp, bottom = 100.dp, start = 16.dp, end = 16.dp)
         .border(width = 2.dp, color = Color.White, shape = RoundedCornerShape(30.dp))) {
-        Column(Modifier.verticalScroll(rememberScrollState()),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center) {
+        ConstraintLayout{
+            val (image, nameText, locationText, rowStats, buttonFollow, buttonMessage) = createRefs()
+            val guideLine = createGuidelineFromTop(0.3f)
+
             Image(
                 painter = painterResource(id = R.drawable.cat1),
                 contentDescription = "cat1",
                 modifier = Modifier
-                    .size(100.dp)
+                    .size(200.dp)
                     .clip(CircleShape)
                     .border(
                         width = 2.dp,
                         color = Color.Red,
                         shape = CircleShape
-                    ),
+                    ).constrainAs(image){
+                        top.linkTo(guideLine)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
                 contentScale = ContentScale.Crop)
 
-            Text(text="Thoughtful cat")
-            Text(text="Forest")
+            Text(text="Thoughtful cat", modifier = Modifier.constrainAs(nameText){
+                top.linkTo(image.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            })
+            Text(text="Forest", modifier = Modifier.constrainAs(locationText){
+                top.linkTo(nameText.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            })
 
             Row(horizontalArrangement = Arrangement.SpaceEvenly,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp)) {
+                    .padding(16.dp)
+                    .constrainAs(rowStats){
+                        top.linkTo(locationText.bottom)
+                    }) {
                 ProfileStats(count = "150", title = "Followers")
                 ProfileStats(count = "100", title = "Following")
                 ProfileStats(count = "30", title = "Posts")
             }
 
-            Row(horizontalArrangement = Arrangement.SpaceEvenly,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)) {
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "Follow User")
-                }
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "Direct Message")
-                }
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.constrainAs(buttonFollow){
+                top.linkTo(rowStats.bottom, margin = 16.dp)
+                start.linkTo(parent.start)
+                end.linkTo(buttonMessage.start)
+                width = Dimension.wrapContent
+            }) {
+                Text(text = "Follow User")
+            }
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.constrainAs(buttonMessage){
+                top.linkTo(rowStats.bottom, margin = 16.dp)
+                start.linkTo(buttonFollow.end)
+                end.linkTo(parent.end)
+                width = Dimension.wrapContent
+            }) {
+                Text(text = "Direct Message")
             }
 
         }
